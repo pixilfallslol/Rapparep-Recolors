2c4ee3f7637b59dde80b2296b5ab9a77b4ac2a27
Pixil <yayitstoday22@gmail.com>
2026-02-12 22:40:06 -0600

    the site

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var imgs = ["https://i.imgur.com/9kOaC1a.png","https://i.imgur.com/n9bL0tz.png",
 var layers = [];
 var colors = [];
 
+var stretch = 0;
+
 function preload(){
   for(let i = 0; i < imgs.length; i++){
     layers[i] = loadImage(imgs[i]);
@@ -17,10 +19,12 @@ function setup(){
     colors[i] = color(random(255),random(255),random(255));
   }
   let CANVAS = createCanvas(W_W, W_H);
+  imageMode(CENTER);
 }
 
 function draw(){
   background(0xff);
+  stretch*=0.9;
   drawLayers();
   drawBase();
 }
@@ -28,16 +32,17 @@ function draw(){
 function drawLayers(){
   for(let i = 1; i < layers.length; i++){
     tint(colors[i]);
-    image(layers[i],0,0);
+    image(layers[i],200,200,layers[i].width-stretch,stretch+layers[i].height);
   }
   noTint();
 }
 
 function drawBase(){
-  image(layers[0],0,0);
+  image(layers[0],200,200,layers[0].width-stretch,stretch+layers[0].height);
 }
 
 function mousePressed(){
+  stretch = 100-(stretch/3);
   for(let i = 0; i < imgs.length; i++){
     colors[i] = color(random(255),random(255),random(255));
   }
